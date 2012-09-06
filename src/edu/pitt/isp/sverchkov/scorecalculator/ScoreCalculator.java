5234479e344d015c3789e5f0f1d6e8a8b493f5f1
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2012-09-06 12:12:05 -0400

    Fixed improper probability calculation bug

---
+++ src/edu/pitt/isp/sverchkov/scorecalculator/ScoreCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pitt.isp.sverchkov.scorecalculator;
+
+/**
+ *
+ * @author YUS24
+ */
+public class ScoreCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
