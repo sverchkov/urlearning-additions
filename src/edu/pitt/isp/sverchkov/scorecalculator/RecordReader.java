a2d1f4e4c1113a001907b34d1ffba2da271d2278
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2012-09-18 17:20:42 -0400

    Implemented RecordReader

--- src/edu/pitt/isp/sverchkov/scorecalculator/RecordReader.java
+++ src/edu/pitt/isp/sverchkov/scorecalculator/RecordReader.java
@@ -4,14 +4,37 @@
  */
 package edu.pitt.isp.sverchkov.scorecalculator;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
  * @author YUS24
  */
 class RecordReader {
-    public static RecordSet read( File recordFile ){
-        return null;
+    public static RecordSet read( File recordFile ) {
+        
+        // Read into arrays+lists
+        String splitter = "\\s*,\\s*";
+        String[] varnames = null;
+        List<List<String>> rows = new ArrayList<>();
+        try( Scanner in = new Scanner( new BufferedReader( new FileReader( recordFile ) ) ) ){
+            varnames = in.nextLine().split(splitter);
+            while( in.hasNext() ){
+                rows.add( Arrays.asList( in.nextLine().split(splitter) ) );                
+            }
+        }catch( IOException e ){}
+        
+        // Convert lists to arrays
+        String[][] data = new String[rows.size()][];
+        for( int i=0; i<data.length; i++ )
+            data[i] = rows.get(i).toArray( new String[0] );
+        
+        RecordSet result = null;
+        if( varnames != null ) result = new RecordSetImpl( varnames, data );
+        return result;
     }
 }
