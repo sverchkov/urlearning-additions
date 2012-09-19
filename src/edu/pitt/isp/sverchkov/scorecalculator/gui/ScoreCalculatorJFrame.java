b9c495bb13553f18032b926e37ed6f7cda8ee89d
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2012-09-19 14:32:37 -0400

    Made the "submit" button functional and added a hacky lock to make the caller wait

--- src/edu/pitt/isp/sverchkov/scorecalculator/gui/ScoreCalculatorJFrame.java
+++ src/edu/pitt/isp/sverchkov/scorecalculator/gui/ScoreCalculatorJFrame.java
@@ -16,13 +16,15 @@ public class ScoreCalculatorJFrame extends javax.swing.JFrame {
     private File recordFile;
     private File outputFile;
     private double ess;
+    private final Object lock;
 
     /**
      * Creates new form ScoreCalculatorJFrame
      */
-    public ScoreCalculatorJFrame() {
+    public ScoreCalculatorJFrame( Object lock ) {
         initComponents();
         jTextField1.setInputVerifier( new DecimalInputVerifier() );
+        this.lock = lock;
     }
 
     /**
@@ -72,7 +74,8 @@ public class ScoreCalculatorJFrame extends javax.swing.JFrame {
             }
         });
 
-        jRadioButton1.setText("BDe");
+        jRadioButton1.setSelected(true);
+        jRadioButton1.setText("BDeu");
         jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jRadioButton1ActionPerformed(evt);
@@ -81,7 +84,7 @@ public class ScoreCalculatorJFrame extends javax.swing.JFrame {
 
         jLabel6.setText("ESS Hyperparameter:");
 
-        jTextField1.setText("01.00");
+        jTextField1.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextField1ActionPerformed(evt);
@@ -89,6 +92,11 @@ public class ScoreCalculatorJFrame extends javax.swing.JFrame {
         });
 
         jButton3.setText("Submit");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -110,19 +118,18 @@ public class ScoreCalculatorJFrame extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton2))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(jRadioButton1)
-                                .addGap(18, 18, 18)
-                                .addComponent(jLabel6)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(0, 165, Short.MAX_VALUE))
+                        .addComponent(jLabel3)
+                        .addGap(0, 305, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton3)))
+                        .addComponent(jButton3))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jRadioButton1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField1)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -181,6 +188,12 @@ public class ScoreCalculatorJFrame extends javax.swing.JFrame {
         jLabel4.setText( recordFile.getName() );
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        setVisible(false);
+        synchronized( lock ){ lock.notify(); }
+        dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
     public static void setLookAndFeel(){
         /*
          * Set the Nimbus look and feel
@@ -219,7 +232,7 @@ public class ScoreCalculatorJFrame extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
-                new ScoreCalculatorJFrame().setVisible(true);
+                new ScoreCalculatorJFrame( new Object() ).setVisible(true);
             }
         });
     }
